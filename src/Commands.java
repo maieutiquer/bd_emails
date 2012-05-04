6d23afab51bbd05030d2dbc6be0bc5676e196983
Daniel Rodriguez <maieutiquer@gmail.com>
2012-05-04 07:37:49 +0200

    finished the order bug

--- src/Commands.java
+++ src/Commands.java
@@ -73,11 +73,11 @@ public class Commands {
 	    	if(counter.countFromWhere("isp_domains_2", "`name`='"+domains[i]+"'")==0){
 	    		if(counter.countFromWhere(clrefDomainsTable, "cl_ref="+companies[i])==0){
 	    			domains[i] = domains[i].replaceAll("'", "\\\\'"); // the char ' is replaced with \'
-	    			insert.insertRow(clrefDomainsTable, "`cl_ref_original`, `nom_domaine`", "'"+companies[i]+"', '"+domains[i]+"'");
-	    			insertedNewClref++;
+	    			insert.insertRow(clrefDomainsTable, "`cl_ref`, `cl_ref_original`, `nom_domaine`", "'"+companies[i]+"', '"+companies[i]+"', '"+domains[i]+"'");
 	    		} else if ((counter.countFromWhere(clrefDomainsTable, "(`cl_ref`='"+companies[i]+"' AND `nom_domaine`='"+domains[i]+"')")==0)) {
 	    			domains[i] = domains[i].replaceAll("'", "\\\\'"); // the char ' is replaced with \'
-	    			insert.insertRow(clrefDomainsTable, "`cl_ref`, `cl_ref_original`, `nom_domaine`", "'"+companies[i]+"', '"+companies[i]+"', '"+domains[i]+"'");
+	    			insert.insertRow(clrefDomainsTable, "`cl_ref_original`, `nom_domaine`", "'"+companies[i]+"', '"+domains[i]+"'");
+	    			insertedNewClref++;
 	    		}
 	    	}
 	    }
