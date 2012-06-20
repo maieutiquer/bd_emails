f9330b95b725bfc4408a5375954347303ef57483
Daniel Rodriguez <maieutiquer@gmail.com>
2012-06-20 19:40:07 +0300

    Update master

--- src/BdEmails.java
+++ src/BdEmails.java
@@ -12,12 +12,12 @@
  * 
  */
 public class BdEmails {
-	
+
 	private static String url = "jdbc:mysql://localhost:3306/"; //DB name
 	private static String dbName = "bd_emails"; //DB name
 	private static String user = "root"; //DB username
 	private static String pwd = "asd123"; //DB password
-	
+
 	/**
 	 * Methods are called from here with the desired parameters.
 	 * 
@@ -32,15 +32,15 @@ public class BdEmails {
 		}
 		Commands commands = new Commands(url+dbName, user, pwd);
 		commands.openConnection();
-		
+
 //		commands.showColumnNames("bd_emails_03");
-		
+
 //		insertNonEmpty("bd_emails", "bd_emails_05");
 //		insertEmpty("bd_emails_05","empty_deleted");
-		
+
 //		commands.copyEmpty("bd_emails_03", "empty_deleted_3");
 //		commands.copyNonEmpty("bd_emails_03", "bd_emails_temp3");
-		
+
 //		commands.countAll("empty_deleted_3");
 //		System.out.println();
 //		commands.countAll("bd_emails_05");
@@ -51,7 +51,7 @@ public class BdEmails {
 //		commands.countEmptyLastname("bd_emails_05");
 //		commands.countEmptyEmail("bd_emails_05");
 //		commands.countEmptyGender("bd_emails_05");
-		
+
 //		commands.insertDistinctDomains("bd_emails_05", "domains_1");
 //		commands.cloneTable("domains_1", "domains_2");
 //		commands.updateDomainUsersNumber("bd_emails_05", "domains_2");
@@ -61,30 +61,30 @@ public class BdEmails {
 //		commands.countAll("domains_3");
 //		commands.countDomainsWithXCompanies("domains_3", 0);
 //		commands.countDomainsWithXCompanies("domains_3", 1);
-		
+
 //		commands.countDomainsWithOneAddress("bd_emails_05", "domains_3");
-		
+
 //		commands.updateIspDomainUsersNumber("bd_emails_05", "isp_domains_2");
-		
+
 //		commands.countDistinct("bd_emails_06", "cl_ref");
 //		commands.createMoreClref("bd_emails_06", "cl_ref_domains_3");
 //		commands.countAll("cl_ref_domains_3");
 //		commands.countDistinct("cl_ref_domains_3", "cl_ref_original");
 //		
 //		commands.countDedoubledClients("cl_ref_domains_2");
-		
+
 //		commands.convertDomainsToLowercase("bd_emails_05_autres_1");
 //		commands.convertEmailsToLowercase("bd_emails_05_autres_1");
 //		commands.cleanClrefs("bd_emails_05_autres_1");
 //		commands.createMoreClref("bd_emails_05_autres_1", "cl_ref_domains_4");
-		
+
 //		commands.copyClientsWithOtherDomains("bd_emails_05", "bd_emails_05_autres");
 //		commands.copyClientsWithIspDomains("bd_emails_05", "bd_emails_05_isp");
 //		commands.countAll("bd_emails_05_isp");
-		
+
 //		commands.determineUser("bd_emails_05_autres_2");
-		commands.countAll("bd_emails_05_autres_5");
-		
+		commands.countAll("bd_emails_05_autres_6");
+
 //		commands.determineUserAndDomain("bd_emails_05_isp_3");
 //		
 //		commands.countEmptyFirstname("bd_emails_05_isp_3");
@@ -93,13 +93,15 @@ public class BdEmails {
 //		commands.countEmptyDomain("bd_emails_05_isp_3");
 //		commands.countEmptyEmail("bd_emails_05_isp_3");
 //		commands.countEmptyGender("bd_emails_05_isp_3");
-		
-//		commands.determineEmptyUserRule("bd_emails_05_autres_5");
-		commands.determineUserRule("bd_emails_05_autres_5");
-//		commands.determineFirstNameRule("bd_emails_05_autres_5");
-		
+
+//		commands.fillClRefs("bd_emails_05_autres_6", "cl_ref_regles");
+//		commands.determineEmptyUserRule("bd_emails_05_autres_6");
+//		commands.insertHardcodedRules("bd_emails_05_autres_6");
+//		commands.determineUserRules("bd_emails_05_autres_6");
+		commands.countMastersAndSlaves("bd_emails_05_autres_6");
+
 		commands.closeConnection();
 		System.exit(0);
 	}
-	
+
 }
